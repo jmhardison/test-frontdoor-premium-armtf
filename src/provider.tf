e300da6e391461c6573a9e4b7c9d10f0436a3057
Jonathan Hardison <jonathan.hardison@banno.com>
2021-06-01 15:07:59 -0500

    cleaned out some commented items

--- src/provider.tf
+++ src/provider.tf
@@ -1,3 +1,4 @@
+################################################################################################################################################## provider
 provider "azurerm" {
   features {
     key_vault {
@@ -6,24 +7,15 @@ provider "azurerm" {
     }
   }
 }
-/*
-provider "azurerm" {
-  alias   = "shared_sub"
-  subscription_id      = var.Shrd_SubID
-  features {}
-}
-*/
+
+################################################################################################################################################## current context
 
 data "azurerm_client_config" "current" {
 }
 
+################################################################################################################################################## backend
 //backend configuration///////////
 terraform {
   #lock tf version to 0.14.10 or newer
   required_version = ">=0.14.10"
-  #   backend "azurerm" {
-  #     storage_account_name = "cldsharedstate"
-  #     container_name       = "tf-prod-profitstars-jhlm-azr"
-  #     key                  = "prod.new.terraform.tfstate"
-  #   }
 }
